53d466ff4f766d2d7506f28769b77f3327c66f2f
yara <uraeel@gmail.com>
2024-06-06 18:07:37 -0400

    App.js routes updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 //client/src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PostList from './pages/PostList';
-import PostForm from './pages/PostForm';
-import Login from './pages/Login';
-import PrivateRoute from './pages/PrivateRoute';
-import Navbar from './components/Navbar';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PostList from "./pages/PostList";
+import PostForm from "./pages/PostForm";
+import Login from "./pages/Login";
+import PrivateRoute from "./pages/PrivateRoute";
+import Navbar from "./components/Navbar";
+import PostDetail from "./components/PostDetail";
+import "./App.css";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
+          <Route path="/posts/:id" element={<PostDetail />} />
           <Route path="/login" element={<Login />} />
           <Route path="/myblog" element={<PostList />} />
           <Route path="/" element={<PrivateRoute />}>
