6155e6c831538817e8c4ce89232c3372b9c4a6ab
yara <uraeel@gmail.com>
2024-06-12 19:31:34 -0400

    change privetroute navigate to /myblog root

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,7 +5,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = () => {
   const isAuth = !!localStorage.getItem('token');
-  return isAuth ? <Outlet /> : <Navigate to="/login" />;
+  return isAuth ? <Outlet /> : <Navigate to="/myblog" />;
 };
 
 export default PrivateRoute;
