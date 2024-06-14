fcc018fe855b2db547a97395526d0d7ffa514c49
yara <uraeel@gmail.com>
2024-06-14 00:08:10 -0400

    test

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,10 +2,9 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-
-const PrivateRoute = () => {
+const PrivateRoute = ({ shouldNavigate = false }) => {
   const isAuth = !!localStorage.getItem('token');
-  return isAuth ? <Outlet /> : <Navigate to="/myblog" />;
+  return isAuth ? <Outlet /> : shouldNavigate ? <Navigate to="/login" /> : null;
 };
 
 export default PrivateRoute;
