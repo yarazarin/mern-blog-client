dc4ffee68efbec51ccc06f29d2f0f3e524eae4e9
yara <uraeel@gmail.com>
2024-06-20 14:34:42 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import PostList from "./pages/PostList";
-import PostForm from "./pages/PostForm";
-import Login from "./pages/Login";
-import PrivateRoute from "./pages/PrivateRoute";
-import Navbar from "./components/Navbar";
-import PostDetail from "./components/PostDetail";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navbar';
+import Home from './pages/PostList';
+import PostDetail from './components/PostDetail';
+import PostForm from './pages/PostForm';
+import Login from './pages/Login';
+import PrivateRoute from './pages/PrivateRoute';
+import './App.css';
 
 function App() {
   return (
     <Router basename="/mern-blog-client">
       <div className="App">
-        <Navbar />
+        <Navigation />
         <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/myblog" element={<Home />} />
           <Route path="/posts/:id" element={<PostDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/myblog" element={<PostList />} />
           <Route path="/new" element={<PrivateRoute />}>
             <Route path="/new" element={<PostForm />} />
           </Route>
           <Route path="/edit/:id" element={<PrivateRoute />}>
             <Route path="/edit/:id" element={<PostForm />} />
           </Route>
-          <Route path="/" element={<PostList />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
