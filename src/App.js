e68b3dd8751169b6b0ce75c28f9200b6d7cdf4c6
yara <uraeel@gmail.com>
2024-06-20 11:43:04 -0400

    using HashRouter for github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import PostList from "./pages/PostList";
+// import PostForm from "./pages/PostForm";
+// import Login from "./pages/Login";
+// import PrivateRoute from "./pages/PrivateRoute";
+// import Navbar from "./components/Navbar";
+// import PostDetail from "./components/PostDetail";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <Router basename="/mern-blog-client">
+//       <div className="App">
+//         <Navbar />
+//         <Routes>
+//           <Route path="/posts/:id" element={<PostDetail />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/myblog" element={<PostList />} />
+//           <Route path="/new" element={<PrivateRoute />}>
+//             <Route path="/new" element={<PostForm />} />
+//           </Route>
+//           <Route path="/edit/:id" element={<PrivateRoute />}>
+//             <Route path="/edit/:id" element={<PostForm />} />
+//           </Route>
+//           <Route path="/" element={<PostList />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";// HashRouter for github
 import PostList from "./pages/PostList";
 import PostForm from "./pages/PostForm";
 import Login from "./pages/Login";
@@ -10,7 +45,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/mern-blog-client">
+    <Router>
       <div className="App">
         <Navbar />
         <Routes>
